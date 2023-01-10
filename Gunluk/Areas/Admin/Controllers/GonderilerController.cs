8c4f5b34d706e89a6f7f1591ce5c9a186f2b8dc0
Akın Özçelik <110735711+akinozcelik0@users.noreply.github.com>
2023-01-10 16:02:39 +0300

    Listing of Gonderi edited. Delete and update buttons added. Modal system for delete added.

--- Gunluk/Areas/Admin/Controllers/GonderilerController.cs
+++ Gunluk/Areas/Admin/Controllers/GonderilerController.cs
@@ -59,8 +59,67 @@ namespace Gunluk.Areas.Admin.Controllers
         //DUZENLE-----------=-=-=-=-=-=-=
         public IActionResult Duzenle(int id)
         {
+            var gonderi = _db.Gonderiler.Find(id);
+
+            if (gonderi == null)
+                return NotFound();
+
+            var vm = new GonderiViewModel()
+            {
+                Id = gonderi.Id,
+                Baslik = gonderi.Baslik,
+                Icerik = gonderi.Icerik,
+                KategoriId = gonderi.KategoriId
+
+            };
+
+            KategorileriYukle();
+            return View("Yonet",vm);
+        }
+
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Duzenle(GonderiViewModel vm)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var gonderi = _db.Gonderiler.Find(vm.Id);
+
+                if (gonderi == null)
+                {
+                    return NotFound();
+                }
+
+                gonderi.Baslik = vm.Baslik;
+                gonderi.Icerik = vm.Icerik;
+                gonderi.DegistirilmeZamani = DateTime.Now;
+                gonderi.KategoriId = vm.KategoriId!.Value;
+                
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index), new { durum = "duzenlendi" });
+            }
+            KategorileriYukle();
             return View("Yonet");
         }
+
+        [HttpPost,ValidateAntiForgeryToken]
+        public IActionResult Sil(int id)
+        {
+            var gonderi = _db.Gonderiler.Find(id);
+
+            if (gonderi == null)
+            {
+                return NotFound();
+            }
+
+            _db.Gonderiler.Remove(gonderi);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index), new { durum = "silindi" });
+        }
+
+
 
 
 
